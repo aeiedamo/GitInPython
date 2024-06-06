e24081317e3b43e3c1ef5f7f3f46bbc9ddd34237
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-06-06 08:04:18 +0100

    add hash-object

--- libgip.py
+++ libgip.py
@@ -382,3 +382,55 @@ class GitBlob(GitObject):
 
     def deserialize(self, data):
         self.blobdata = data
+
+
+argsp = argsubparsers.add_parser("hash-object", help="Compute object id and sha")
+
+argsp.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default="blob",
+    help="Specify the type of object",
+)
+
+argsp.add_argument(
+    "-w", dest="write", action="store_true", help="write Object to database"
+)
+
+argsp.add_argument("path", help="Read object from <file>")
+
+
+def cmd_hashobject(args):
+    """
+    The base kickstarter for hash-object
+    """
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as f:
+        sha = object_hash(f, args.type.encode(), repo)
+        print(sha)
+
+
+def object_hash(f, fmt, repo=None):
+    """
+    to hash an object, to write it to repository if needed
+    """
+    data = f.read()
+
+    match fmt:
+        case b"commit":
+            obj = GitCommit(data)
+        case b"tree":
+            obj = GitTree(data)
+        case b"tag":
+            obj = GitTag(data)
+        case b"blob":
+            obj = GitBlob(data)
+        case _:
+            raise Exception("Unknown type: {}".format(fmt))
+    return object_write(obj, repo)
